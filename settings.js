970f54b7764814deb122d092febe77d75ccdc6d0
AgroTechGenius <olajidesherif05@gmail.com>
2025-07-01 23:33:05 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Lucky Tech Hub Bot',
+  author: '‎',
+  botName: "Telebotdev Bot",
+  botOwner: 'Sherif', // Your name
+  ownerNumber: '2349076129380', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.8",
+};
+
+module.exports = settings;
